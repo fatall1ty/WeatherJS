f9b82ca05a850f69618df469c3c9956b6afa4a87
Your Name <you@example.com>
2020-05-09 20:19:16 +0200

    Save location to storage

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+//Init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
 //Init weather object
-const weather = new Weather('Biała Podlaska', 'PL');
-
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 //Init UI
 const ui = new UI(weather);
+
 // Get weather on DOM Load
 document.addEventListener('DOMContentLoaded', getWeather);
+
+// Change Location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+    weather.changeLocation(city, state);
+    getWeather();
+
+    //Set location in LS
+    storage.setLocationData(city, state)
+    $('#locModal').modal('hide');
+})
 
 function getWeather() {
     weather.getWeather().then(results => {
